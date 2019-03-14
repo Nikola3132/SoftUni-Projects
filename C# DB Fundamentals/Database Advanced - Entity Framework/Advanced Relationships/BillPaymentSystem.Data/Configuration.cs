e8ded68354188312f692bea7055e4ab408528760
Nikola Dimitrov <ndimitrov032@gmail.com>
2019-03-14 16:58:40 +0200

    Advanced Relationships

---
+++ C# DB Fundamentals/Database Advanced - Entity Framework/Advanced Relationships/BillPaymentSystem.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace BillPaymentSystem.Data
+{
+    public class Configuration
+    {
+        public const string ConnectionString = @"Server=DESKTOP-JU304LN\SQLEXPRESS;Database=Sales;Integrated Security=true";
+    }
+}
